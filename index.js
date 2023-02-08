626ff127d2dcc800bc9c98af90642356b061bdea
kutluhann <kutluhan.sipka@ug.bilkent.edu.tr>
2023-02-08 13:44:13 +0300

    Add yarn and express

---
+++ index.js
@@ -0,0 +1,11 @@
+require("dotenv").config()
+const express = require("express")
+const app = express()
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.json({status: 200})
+}) 
+
+app.listen(PORT)
